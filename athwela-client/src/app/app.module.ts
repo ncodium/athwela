9a9f561276cdf59e119d2e0aae99cd10b1c563f4
Anjula Karunarathne <anjulakarunarathne@gmail.com>
2019-08-25 17:12:14 +0530

    Slight code refactoring

--- athwela-client/src/app/app.module.ts
+++ athwela-client/src/app/app.module.ts
@@ -3,20 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// manual imports
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// custom components
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
+
+// custom services
+import { ValidateService } from './services/validate.service';
+
+// ngx-bootstrap modules
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { AlertModule } from 'ngx-bootstrap/alert';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { ValidateService } from './services/validate.service';
-
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -37,8 +43,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     BrowserAnimationsModule,
     FormsModule
   ],
-  providers: [ValidateService],
-  bootstrap: [AppComponent],
-  entryComponents: [ LoginComponent, RegisterComponent ]
+  providers: [
+    ValidateService
+  ],
+  bootstrap: [
+    AppComponent
+  ],
+  entryComponents: [
+    LoginComponent,
+    RegisterComponent
+  ]
 })
 export class AppModule { }
