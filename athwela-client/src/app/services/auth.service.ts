a2c5d8e385c1d83d749f8a469209195ec90c1e73
Piyumal Karunathilaka <piyuk.thilaka@gmail.com>
2019-08-26 23:54:56 +0530

    Implemented auth service

---
+++ athwela-client/src/app/services/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+// import 'rxjs/add/operator/map';
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  authToken: any;
+  user: any;
+
+  constructor(private http: HttpClient) { }
+
+  registerUser(user){
+    let headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost:3000/users/register', user, {headers: headers})
+    // .map(res => res.json());
+    .pipe(map(res => res.json()));
+  }
+}
