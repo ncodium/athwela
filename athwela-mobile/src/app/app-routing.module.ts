9742f9c27d32568d5abe2918b2498666a2390476
Anjula Karunarathne <anjulakarunarathne@gmail.com>
2019-08-29 01:06:48 +0530

    Updated NS and reinitiated app with a template

--- athwela-mobile/src/app/app-routing.module.ts
+++ athwela-mobile/src/app/app-routing.module.ts
@@ -4,7 +4,11 @@ import { NativeScriptRouterModule } from "nativescript-angular/router";
 
 const routes: Routes = [
     { path: "", redirectTo: "/home", pathMatch: "full" },
-    { path: "home", loadChildren: "~/app/home/home.module#HomeModule" }
+    { path: "home", loadChildren: "~/app/home/home.module#HomeModule" },
+    { path: "browse", loadChildren: "~/app/browse/browse.module#BrowseModule" },
+    { path: "search", loadChildren: "~/app/search/search.module#SearchModule" },
+    { path: "featured", loadChildren: "~/app/featured/featured.module#FeaturedModule" },
+    { path: "settings", loadChildren: "~/app/settings/settings.module#SettingsModule" }
 ];
 
 @NgModule({
--- athwela-mobile/src/app/home/home.component.ts
+++ athwela-mobile/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+import * as app from "tns-core-modules/application";
 
 @Component({
     selector: "Home",
@@ -13,5 +15,10 @@ export class HomeComponent implements OnInit {
 
     ngOnInit(): void {
         // Init your component properties here.
+    }
+
+    onDrawerButtonTap(): void {
+        const sideDrawer = <RadSideDrawer>app.getRootView();
+        sideDrawer.showDrawer();
     }
 }
