809654dcd1bb27cf6890565fdad0840da931383c
Piyumal Karunathilaka <piyuk.thilaka@gmail.com>
2019-08-29 16:42:17 +0530

    Minor html fixes

--- athwela-server/app.js
+++ athwela-server/app.js
@@ -44,6 +44,9 @@ app.get('/', (req, res) => {
     res.send('Invalid Endpoint');
 });
 
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public/index.html'));
+// });
 
 app.listen(port, () => {
     console.log('Server started on port ' + port);
