f18a22b28c0452954dc3d7d923606191e83f9dcc
Anjula Karunarathne <anjulakarunarathne@gmail.com>
2019-09-01 20:01:44 +0530

    Reduced navbar shadow

--- athwela-client/src/app/app-routing.module.ts
+++ athwela-client/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import { AdminComponent } from './components/admin/admin.component';
 import { AuthGuard } from './guards/auth.guard';
 
 
@@ -11,7 +10,6 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
-  { path: 'admin', component: AdminComponent }
 ];
 
 @NgModule({
