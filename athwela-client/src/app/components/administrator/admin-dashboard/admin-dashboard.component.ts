9de9fb321473f12cdc19018a8d2002b3dc4ebcbc
Anjula Karunarathne <anjulakarunarathne@gmail.com>
2019-09-07 21:26:31 +0530

    Fixed charts not appearing due to missing data in controller

--- athwela-client/src/app/components/administrator/admin-dashboard/admin-dashboard.component.ts
+++ athwela-client/src/app/components/administrator/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
+import { SingleDataSet } from 'ng2-charts';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -6,6 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-dashboard.component.scss']
 })
 export class AdminDashboardComponent implements OnInit {
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public barChartLabels: Label[] = ['jan', 'feb', 'march', 'april', 'may', 'june', 'july'];
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+  public barChartPlugins = [];
+
+  public barChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'donations' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'requests' }
+  ];
+  public polarAreaChartLabels: Label[] = ['Total requests', 'Verified requests', 'Approved requests', 'Diclined requests', 'Pending Requests'];
+  public polarAreaChartData: SingleDataSet = [700, 500, 300, 150, 50];
+  public polarAreaLegend = true;
+
+  public polarAreaChartType: ChartType = 'polarArea';
 
   constructor() { }
 
