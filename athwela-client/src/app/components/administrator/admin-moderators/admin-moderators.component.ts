a423b319656395a17955c86bd314d4389ce1d2f8
RashiniMaduka <43130483+RashiniMaduka@users.noreply.github.com>
2019-09-19 21:57:12 +0530

    campaigns page

--- athwela-client/src/app/components/administrator/admin-moderators/admin-moderators.component.ts
+++ athwela-client/src/app/components/administrator/admin-moderators/admin-moderators.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {  TemplateRef } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { ChartDataSets, ChartType, ChartOptions } from 'chart.js';
 
 
 @Component({
@@ -9,6 +10,24 @@ import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
   styleUrls: ['./admin-moderators.component.scss']
 })
 export class AdminModeratorsComponent implements OnInit {
+  public scatterChartOptions: ChartOptions = {
+    responsive: true,
+  };
+
+  public scatterChartData: ChartDataSets[] = [
+    {
+      data: [
+        { x: 1, y: 1 },
+        { x: 2, y: 3 },
+        { x: 3, y: -2 },
+        { x: 4, y: 4 },
+        { x: 5, y: -3, r: 20 },
+      ],
+      label: 'Series A',
+      pointRadius: 10,
+    },
+  ];
+  public scatterChartType: ChartType = 'scatter';
   
   modalRef: BsModalRef;
   constructor(private modalService: BsModalService) {}
