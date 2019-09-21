6c12b50d8960235bab38508954fd954d980c8729
Anjula Karunarathne <anjulakarunarathne@gmail.com>
2019-09-21 23:41:54 +0530

    Implemnted create campaign functionality in campaign service passing jwt token

--- athwela-client/src/app/services/campaign.service.ts
+++ athwela-client/src/app/services/campaign.service.ts
@@ -1,23 +1,41 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from "rxjs/operators";
 
 import { Campaign } from '../models/campaign.model';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CampaignService {
-  selectedCampaign: Campaign;
-  campaigns: Campaign[];
   readonly baseURL = 'http://localhost:3000/campaigns';
 
-  constructor(private http: HttpClient) { }
+  selectedCampaign: Campaign;
+  campaigns: Campaign[];
+
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+  ) { }
 
   getCampaignList() {
     return this.http.get(this.baseURL);
   }
 
-  
+  createCampaign(campaign: Campaign) {
+    this.authService.loadToken();
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': this.authService.authToken
+      })
+    };
+
+    return this.http.post(this.baseURL, campaign, httpOptions).pipe();
+  }
+
+
 }
