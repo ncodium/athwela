695cad45ded5967783a0e6198928dd267c2b05b4
Piyumal Karunathilaka <piyuk.thilaka@gmail.com>
2019-11-05 14:22:00 +0530

    Implementing form group

--- athwela-client/src/app/components/campaigns-new/campaigns-new.component.ts
+++ athwela-client/src/app/components/campaigns-new/campaigns-new.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CampaignService } from '../../services/campaign.service';
 import { Router } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
@@ -14,11 +14,13 @@ export class NewCampaignComponent implements OnInit {
   id: string;
   categories: string[] = ["medical", "education"]
 
-  name = new FormControl('');
-  description = new FormControl('');
-  target = new FormControl('');
-  deadline = new FormControl('');
-  category = new FormControl('');
+  campaignForm: FormGroup;
+
+  // name = new FormControl('');
+  // description = new FormControl('');
+  // target = new FormControl('');
+  // deadline = new FormControl('');
+  // category = new FormControl('');
 
   constructor(
     private campaignService: CampaignService,
@@ -27,26 +29,37 @@ export class NewCampaignComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-  }
-
-  onCreateCampaign() {
-    const campaign = {
-      name: this.name.value,
-      description: this.description.value,
-      target: this.target.value,
-      deadline: this.deadline.value,
-      raised: 0,
-      category: this.category.value
-    }
-
-    // register User
-    this.campaignService.createCampaign(campaign).subscribe(data => {
-      if (data['success']) {
-        const campaignId: string = data['campaign']['_id'];
-        this.router.navigate([`/campaign/${campaignId}`]);
-      } else {
-        // show error
-      }
+    this.campaignForm = new FormGroup({
+      name: new FormControl(),
+      description: new FormControl(),
+      target: new FormControl(),
+      deadline: new FormControl(),
+      raised: new FormControl(),
+      category: new FormControl()
     });
   }
+
+  // onCreateCampaign() {
+  //   const campaign = {
+  //     name: this.name.value,
+  //     description: this.description.value,
+  //     target: this.target.value,
+  //     deadline: this.deadline.value,
+  //     raised: 0,
+  //     category: this.category.value
+  //   }
+  onCreateCampaign(): void {
+    console.log(this.campaignForm);
+  }
+
+  // register User
+  // this.campaignService.createCampaign(campaign).subscribe(data => {
+  //   if (data['success']) {
+  //     const campaignId: string = data['campaign']['_id'];
+  //     this.router.navigate([`/campaign/${campaignId}`]);
+  //   } else {
+  //     // show error
+  //   }
+  // });
 }
+
