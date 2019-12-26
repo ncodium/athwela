461f259b0745cabec4fde72d6a85232e83d62046
RashiniMaduka <rashinimaduka1029@gmail.com>
2019-12-26 20:00:56 +0530

    Updated user.service.ts

--- athwela-client/src/app/services/user.service.ts
+++ athwela-client/src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+ 
 import { AuthService } from './auth.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -16,4 +17,10 @@ export class UserService {
   getUser(id: String) {
     return this.http.get(this.baseURL + 'profile/' + id)
   }
+  registermod(user) {
+    let headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost:3000/users/register/moderator', user, { headers: headers }).pipe();
+  }
+
 }
