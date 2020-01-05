d8070c36ad4440c35adc7584f03031bfc56f05d1
Anjula Karunarathne <anjulakarunarathne@gmail.com>
2020-01-05 21:21:11 +0530

    Repository .gitignore fix

--- athwela-client/src/app/config/app-config.ts
+++
@@ -1,5 +0,0 @@
-export const AppConfig = {
-    APP_URL: 'http://localhost:4200/',
-    BASE_URL: 'http://localhost:3000/',
-    PAYHERE_MERCHANT: '1212954',
-}
--- athwela-server/config/appconfig.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    url: 'localhost',
-    port: 3000,
-    public: 'public',
-}
