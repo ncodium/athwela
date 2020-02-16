64744c34b582285761977955ba85d9deed4edf81
RashiniMaduka <rashinimaduka1029@gmail.com>
2020-02-16 15:11:03 +0530

    Admin dashboard

--- athwela-client/src/app/pages/admin-reports/admin-reports.component.ts
+++ athwela-client/src/app/pages/admin-reports/admin-reports.component.ts
@@ -3,8 +3,8 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { ReportService } from '../../services/Report.service';
 import { Campaign } from '../../models/campaign.model';
-import { Donation} from '../../models/donation.model';
- import pdfMake from 'pdfmake/build/pdfmake';
+import { Donation } from '../../models/donation.model';
+import pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
@@ -17,9 +17,12 @@ pdfMake.vfs = pdfFonts.pdfMake.vfs;
 })
 export class AdminReportsComponent implements OnInit {
   reportForm: FormGroup;
-  campaigns:Campaign[];
-  donations:Donation[];
- 
+  campaigns: Campaign[];
+  donations: Donation[];
+  alert: any;
+  fromalert: any;
+  toalert: any;
+
 
   constructor(
     private formBuilder: FormBuilder,
@@ -46,81 +49,149 @@ export class AdminReportsComponent implements OnInit {
   }
 
   onSubmitCampaigns() {
-    this.reportservice.getCampaignsbydate(this.fromDate.value, this.toDate.value).subscribe((res) => {
-      
+    //console.log(this.fromDate.invalid, this.toDate.invalid)
+    this.fromalert = this.toalert = null;
+    this.alert=null;
+    if(this.fromDate.invalid||this.toDate.invalid){
+    if (this.fromDate.invalid) {
+      this.fromalert = {
+        type: 'danger',
+        msg: 'fromDate is empty'
+      }
+    }
+    if (this.toDate.invalid) {
+      this.toalert = {
+        type: 'danger',
+        msg: 'toDate is empty'
+      }
+    }
+  }
+  else if(this.fromDate.value>this.toDate.value){
+    this.alert={
+      type:'danger',
+      msg:'fromDate should be less than toDate'
+    }
+  }
+  else{
 
-      this.campaigns= res as Campaign[];
-      
-      this.generatePdf(this.campaigns);
-    });
+    
+
+      this.reportservice.getCampaignsbydate(this.fromDate.value, this.toDate.value).subscribe((res) => {
+
+
+        this.campaigns = res as Campaign[];
+
+        this.generatePdf();
+      });
+    //}
+    // else {
+    //   this.alert = {
+    //     type: 'danger',
+    //     msg: 'fromDate should be less than Todate'
+    //   }
+    // }
+  }
+}
+
+  generatePdf() {
+    var rows = [];
+    rows.push(['Name', 'Description', 'Owner', 'Target', 'Deadline', 'Category']);
+
+
+    for (var i of this.campaigns) {
+      rows.push([i.name, i.description, [i.owner.name + ' ' + i.owner.email], 'Rs.'+i.target, i.deadline.toString(), this.toTitleCase(i.category)]);
+    }
+
+    var docDefinition = {
+      content: [
+
+
+        { text: 'Campaigns Report', fontSize: 20, bold: true, margin: [5, 2, 10, 20] },
+
+        { text: 'From ' + this.fromDate.value + ' To ' + this.toDate.value, fontSize: 15, bold: true, margin: [5, 2, 10, 20] },
+
+
+        {
+          table: {
+
+            // widths: ['*', 100, 200, '*', '*', '*'],
+            body: rows
+          }
+        }
+      ]
+    }
+    pdfMake.createPdf(docDefinition).download();
   }
 
-   generatePdf(campaigns:Campaign[]){
-  var rows = [];
-rows.push(['Name', 'Description', 'Owner','Target', 'Deadline', 'Category']);
+  //print donations
+  onSubmitDonations() {
+    this.fromalert = this.toalert = null;
+    this.alert=null;
+    if(this.fromDate.invalid||this.toDate.invalid){
+      if (this.fromDate.invalid) {
+        this.fromalert = {
+          type: 'danger',
+          msg: 'fromDate is empty'
+        }
+      }
+      if (this.toDate.invalid) {
+        this.toalert = {
+          type: 'danger',
+          msg: 'toDate is empty'
+        }
+      }
+    }
+    else{
+
+    this.reportservice.getDonationsbydate(this.fromDate.value, this.toDate.value).subscribe((res) => {
 
 
-for(var i of this.campaigns) {
-    rows.push([i.name, i.description, [i.owner.name + ' ' + i.owner.email],i.target, i.deadline.toString(), i.category]);
+      this.donations = res as Donation[];
+
+      this.generatePdfDonations(this.donations);
+    });
+  }
 }
 
-var docDefinition = {
-    content: [
+  generatePdfDonations(donations: Donation[]) {
+    var rows = [];
+    rows.push(['Donation Id', 'Amount', 'Status Code', 'Status Message', 'Method']);
 
 
-     {text:'Campaigns Report',fontSize: 20,bold:true,margin: [ 5, 2, 10, 20 ]},
+    for (var i of this.donations) {
+      rows.push([i.donation_id, 'Rs.'+i.amount, i.status_code, i.status_message, i.method]);
+    }
 
-     {text:'From '+this.fromDate.value+'To '+this.toDate.value,fontSize: 15 ,bold: true,margin: [ 5, 2, 10, 20 ]},
+    var docDefinition = {
+      content: [
 
 
-        {table: {
+        { text: 'Donations Report', fontSize: 20, bold: true, margin: [5, 2, 10, 20] },
 
-               // widths: ['*', 100, 200, '*', '*', '*'],
-                body: rows
-            }}
-    ]
-}
+        { text: 'From ' + this.fromDate.value + ' To ' + this.toDate.value, fontSize: 15, bold: true, margin: [5, 2, 10, 20] },
+
+
+        {
+          table: {
+
+            // widths: ['*', 100, 200, '*', '*', '*'],
+            body: rows
+          }
+        }
+      ]
+    }
     pdfMake.createPdf(docDefinition).download();
-}
+  }
 
-//print donations
-onSubmitDonations() {
-  this.reportservice.getDonationsbydate(this.fromDate.value, this.toDate.value).subscribe((res) => {
-    
-
-    this.donations= res as Donation[];
-    
-    this.generatePdfDonations(this.donations);
-  });
-}
-
-generatePdfDonations(donations:Donation[]){
-  var rows = [];
-rows.push(['Donation_id','Amount', 'Status_code', 'Status_message','Method']);
-
-
-for(var i of this.donations) {
-    rows.push([i.donation_id,i.amount, i.status_code, i.status_message, i.method]);
-}
-
-var docDefinition = {
-    content: [
-
-
-     {text:'Donations Report',fontSize: 20,bold:true,margin: [ 5, 2, 10, 20 ]},
-
-     {text:'From '+this.fromDate.value+'To '+this.toDate.value,fontSize: 15 ,bold: true,margin: [ 5, 2, 10, 20 ]},
-
-
-        {table: {
-
-               // widths: ['*', 100, 200, '*', '*', '*'],
-                body: rows
-            }}
-    ]
-}
-    pdfMake.createPdf(docDefinition).download();
-}
+  //
+  toTitleCase(str) {
+    return str.replace(
+      /\w\S*/g,
+      function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      }
+    );
+  }
 
 
 }
