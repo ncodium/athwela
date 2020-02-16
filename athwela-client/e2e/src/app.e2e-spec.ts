3346308c46d84e4b492110f6ad5a3765f8ba91d9
Anjula Karunarathne <anjulakarunarathne@gmail.com>
2020-02-16 15:50:15 +0530

    Protractor tests for login and register

--- athwela-client/e2e/src/app.e2e-spec.ts
+++ athwela-client/e2e/src/app.e2e-spec.ts
@@ -31,6 +31,79 @@ describe('workspace-project App', () => {
     expect(page.getAlertText()).toEqual('Registration failed');
   });
 
+  it('should fail if username already exists', () => {
+    page.navigateTo();
+    page.clickRegisterLink();
+    page.clickRegisterButton();
+    page.sendUsername('admin');
+    expect(page.getUsernameAlertText()).toEqual('admin is taken. Try another username.');
+  });
+
+  it('should fail if email format is invalid', () => {
+    page.navigateTo();
+    page.clickRegisterLink();
+    page.clickRegisterButton();
+    page.sendUsername('validusername');
+    page.sendEmail('invalidemail');
+    expect(page.getEmailAlertText()).toEqual('Enter a valid e-mail address.');
+  });
+
+  it('should fail if passwords does not match', () => {
+    page.navigateTo();
+    page.clickRegisterLink();
+    page.clickRegisterButton();
+    page.sendUsername('validusername');
+    page.sendEmail('validemail@gmail.com');
+    page.sendPassword('password1');
+    page.sendConfirmPassword('password2');
+    expect(page.getPasswordAlertText()).toEqual("Your passwords don't match. Please try again.");
+  });
+
+  it('should open sign-in modal', () => {
+    page.navigateTo();
+    page.clickSignInLink();
+    expect(page.getModalTitle()).toEqual('Sign in to Athwela');
+  });
+
+  it('should disable sign-in on invalid submission', () => {
+    page.navigateTo();
+    page.clickSignInLink();
+    expect(page.isSignInButtonEnabled()).toEqual(false);
+  });
+
+  it('should fail if password is missing', () => {
+    page.navigateTo();
+    page.clickSignInLink();
+    page.sendUsername('valid');
+    page.sendPassword('');
+    page.sendUsername('username');
+    expect(page.getInvalidFeedbackAlertText()).toEqual("Please provide your password.");
+  });
+
+  it('should fail if username is missing', () => {
+    page.navigateTo();
+    page.clickSignInLink();
+    page.sendUsername('');
+    page.sendPassword('validpassword');
+    expect(page.getInvalidFeedbackAlertText()).toEqual("Please type in your username.");
+  });
+
+  it('should enable sign-in on valid submission', () => {
+    page.navigateTo();
+    page.clickSignInLink();
+    page.sendUsername('validusername');
+    page.sendPassword('validpassword');
+    expect(page.isSignInButtonEnabled()).toEqual(true);
+  });
+
+  it('should redirect to profile on valid submission', () => {
+    page.navigateTo();
+    page.clickSignInLink();
+    page.sendUsername('user');
+    page.sendPassword('user');
+    page.clickSignInButton();
+    expect(page.getTitle()).toEqual('Anjula Lakshan');
+  });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
--- athwela-client/e2e/src/app.po.ts
+++ athwela-client/e2e/src/app.po.ts
@@ -18,16 +18,64 @@ export class AppPage {
     return element(by.css('.modal-title')).getText() as Promise<string>;
   }
 
+  getTitle() {
+    return element(by.css('.mb-5')).getText() as Promise<string>;
+  }
+
   clickRegisterLink() {
     return element(by.linkText('Register')).click() as Promise<null>;
+  }
+
+  clickSignInLink() {
+    return element(by.linkText('Sign in')).click() as Promise<null>;
   }
 
   clickRegisterButton() {
     return element(by.buttonText('Register')).click() as Promise<null>;
   }
 
+  clickSignInButton() {
+    return element(by.buttonText('Sign in')).click() as Promise<null>;
+  }
+
+  isSignInButtonEnabled() {
+    return element(by.buttonText('Sign in')).isEnabled() as Promise<boolean>;
+  }
+
   getAlertText() {
     return element(by.css('.alert')).getText() as Promise<string>;
+  }
+
+  getUsernameAlertText() {
+    return element(by.css('.invalid-feedback')).getText() as Promise<string>;
+  }
+
+  getEmailAlertText() {
+    return element(by.css('.invalid-feedback')).getText() as Promise<string>;
+  }
+
+  getInvalidFeedbackAlertText() {
+    return element(by.css('.invalid-feedback')).getText() as Promise<string>;
+  }
+
+  getPasswordAlertText() {
+    return element(by.cssContainingText('.invalid-feedback', 'match')).getText() as Promise<string>;
+  }
+
+  sendUsername(username: string) {
+    return element(by.id('inputUsername')).sendKeys(username) as Promise<null>;
+  }
+
+  sendEmail(username: string) {
+    return element(by.id('inputEmail')).sendKeys(username) as Promise<null>;
+  }
+
+  sendPassword(password: string) {
+    return element(by.id('inputPassword')).sendKeys(password) as Promise<null>;
+  }
+
+  sendConfirmPassword(password: string) {
+    return element(by.id('inputConfirmPassword')).sendKeys(password) as Promise<null>;
   }
 
 }
