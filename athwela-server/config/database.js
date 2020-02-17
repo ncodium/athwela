15e487c19c6c2787487d31b5de473a528666c33b
Anjula Karunarathne <anjulakarunarathne@gmail.com>
2020-02-17 06:21:04 +0530

    Added Atlas DB

--- athwela-server/config/database.js
+++ athwela-server/config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/athwelaAuth',
+    database: 'mongodb+srv://labellab:labellab@cluster0-7gnc9.mongodb.net/test?retryWrites=true&w=majority',
     secret: 'athwela'
 }
